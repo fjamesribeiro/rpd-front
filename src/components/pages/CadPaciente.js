9e59f9c45831d55f5632c1a3fa1ec29f9b354196
jamesvianaribeiro <james.ribeiro@orbitinf.com.br>
2023-09-06 17:05:22 -0300

    cors

--- src/components/pages/CadPaciente.js
+++ src/components/pages/CadPaciente.js
@@ -1,14 +1,52 @@
+import { useState } from "react";
 import Input from "../form/Input";
+import styles from "./Home.module.css";
+import SubmitButton from "../form/SubmitButton";
 
 function CadPaciente() {
-    return (
-        <Input
-            name="email"
-            text="Digite o email do paciente"
-            placeHolder="Digite o email do paciente"
-            type="text"
-        />
-    )
+  const [paciente, setPaciente] = useState([]);
+
+  function handleChange(e) {
+    setPaciente({ ...paciente, [e.target.name]: e.target.value });
+  }
+
+  function onSubmit(e) {
+    console.log(paciente);
+    e.preventDefault();
+    const url = process.env.REACT_APP_API_URL;
+
+    fetch(url + "/paciente", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      body: JSON.stringify(paciente),
+    })
+      .then((resp) => resp.json())
+      .then((data) => console.log(data))
+      .catch((err) => console.log(err));
+  }
+
+  return (
+    <form onSubmit={onSubmit} className={styles.home_container}>
+      <Input
+        name="email"
+        text="Digite o email do paciente"
+        placeHolder="Digite o email do paciente"
+        type="text"
+        handleOnChange={handleChange}
+      />
+      <Input
+        name="psicologoId"
+        text="Digite o Código do psicólogo"
+        placeHolder="Digite o email do paciente"
+        type="number"
+        handleOnChange={handleChange}
+        value={paciente.psicologoId}
+      />
+      <SubmitButton text="Cadastrar" />
+    </form>
+  );
 }
 
 export default CadPaciente;
